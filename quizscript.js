4d2740087512ec06a2fba734a2dfa3b683b5e2f7
Courtney Sherman <csherman177@gmail.com>
2023-03-18 21:17:46 -0600

    made a few changes to the code

--- quizscript.js
+++ quizscript.js
@@ -1,23 +1,29 @@
+var startButton = document.getElementById ("start_Button")
+var questionTitle = document.documentqueryselector ("questionTitle")
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+var answerCheck = document.getElementById("answerCheck");
+
 var questions = [ {
   question: "1. This is going to be question #1.",
-  choices: ["msg(Answer 1)", "msg(Answer 2)", "msg(Answer 3)", "msg(Answer 4)", ]
+  choices: ["Answer 1", "msg(Answer 2)", "msg(Answer 3)", "msg(Answer 4)", ],
   correctAnswer: 3
 }
+]
 
-{  question: "2. This is going to be question #2.",
-  choices: ["msg(Answer 1)", "msg(Answer 2)", "msg(Answer 3)", "msg(Answer 4)", ]
+[{  question: "2. This is going to be question #2.",
+  choices: ["msg(Answer 1)", "msg(Answer 2)", "msg(Answer 3)", "msg(Answer 4)", ],
   correctAnswer: 3
 }
 ]
 
 var correctAnswer = 0;
-var startButton = document.queryselector ("start_button")
-
-//display info box
-"startButton".onclick = () =>{
-  "info_box".classList.add("activeInfo");
-}
 
 
-//"startButton".addEventLister ("click", function() { if 
+startButton.addEventLister ("click", startQuiz(){
+  if (startButton.tagName === "startButton")
+}) 
+  
   
